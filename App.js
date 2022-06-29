660e9e0ebbef50f139cdc237cd7a68d59e099eef
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-06-29 17:47:02 +0200

    first

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text styles={styles.subTitle}>starting at 79889$</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {},
+  subTitle: {},
 });
