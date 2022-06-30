5fe42071f999abf2cd0113360288ab9cee3aea22
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-06-30 10:40:49 +0200

    adding buttons

--- components/StyleButton/index.js
+++ components/StyleButton/index.js
@@ -5,9 +5,8 @@ import styles from './styles'
 
 const StyleButton = (props) => {
 
-  const type = props.type;
-  const content = props.content;
-  const onPress = props.onPress;
+
+  const { type , content, onPress } = props;
   
   const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA2';
   const textColor = type === "primary" ? "#FFFFFFA2" : "#171A20CC";
