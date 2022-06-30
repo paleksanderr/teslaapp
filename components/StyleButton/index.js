3fd5d13217d4b708962cc1ad482e4323659a0385
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-06-30 10:20:06 +0200

    adding buttons

--- components/StyleButton/index.js
+++ components/StyleButton/index.js
@@ -6,17 +6,19 @@ import styles from './styles'
 const StyleButton = (props) => {
 
   const type = props.type;
-  const backgroundColor = type === 'primary' ? '#00a8ff' : '#fff';
+  
+  const backgroundColor = type === 'primary' ? 'black' : 'white';
+  const textColor = type === 'primary' ? 'white' : 'black';
 
   return (
     <View style={styles.container}>
       <Pressable
-      style={styles.button}
+      style={[styles.button, { backgroundColor: backgroundColor }]}
       onPress={() => {
         console.warn('pressed')
       }}
       >
-      <Text style={styles.buttonText}>Custom order</Text>
+      <Text style={[styles.buttonText, {color: textColor}]}>Custom order</Text>
       </Pressable>
     </View>
   )
